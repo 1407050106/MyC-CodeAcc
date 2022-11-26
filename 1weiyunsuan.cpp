494640ea0d195770220fc98d83323ef3d25cf340
wangyonglin <2289825383@qq.com>
2022-11-27 01:33:22 +0800

    paixu

---
+++ 1weiyunsuan.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void Print32(int num)
+{
+    for (int i=31; i>=0; i--)      //must 1>=0 not i==0
+    {
+        //cout<<"wyl"<<endl;
+        cout<<((num & (1<<i))==0?0:1);    //must==0?    cout<<((num & (1<<i))==0?0:1)
+    }
+}
+
+int main()
+{
+    // int num=32;
+    // //cout<<"wyl"<<endl;
+    // Print32(num);
+
+    int a=44;
+    Print32(a);
+    cout<<endl;
+    Print32(~a);
+    cout<<endl;
+    cout<<~a+1<<endl;
+
+}
--- paixu.cpp
+++ paixu.cpp
@@ -4,6 +4,40 @@
 #include <algorithm>
 using namespace std;
 
+//xuanze paixu
+void xuanze (vector<int>& v)
+{
+    int num = v.size();
+    if (v.empty() || num <2)
+    {
+        return;
+    }
+
+    for (int i = 0; i<num-1; i++)
+    {
+        int MinIndex = i;
+        for (int j = i+1; j<num; j++)
+        {
+            MinIndex = v[j]<v[MinIndex]?j:MinIndex;
+        }
+        if(MinIndex!=i)                     //zheju buyao yexing 
+        {
+            int temp = v[MinIndex];
+            v[MinIndex] = v[i];
+            v[i]=temp;
+        }
+    }
+    
+}
+
+void swap2(vector<int>& v, int j, int MinIndex)
+{
+    int temp = v[MinIndex];
+    v[MinIndex] = v[j];
+    v[j]=temp;
+}
+
+//maopao paixu
 bool maopao (vector<int>& v)
     {
         int scape = v.size();
@@ -45,9 +79,11 @@ int main() {
     shuzu.push_back(2);
 
     
-    if (maopao(shuzu)) {
-        for_each(shuzu.begin(), shuzu.end(), myway());
-    }
-    
+    // if (maopao(shuzu)) {
+    //     for_each(shuzu.begin(), shuzu.end(), myway());
+    // }
+
+    xuanze(shuzu);
+    for_each(shuzu.begin(), shuzu.end(), myway());
     
 }
