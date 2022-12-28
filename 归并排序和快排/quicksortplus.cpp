0a9718856ba4d8a04bd0b08cdab8468f895e7df9
1407050106 <2289825383@qq.com>
2022-12-28 17:52:17 +0800

    test

--- 归并排序和快排/quicksortplus.cpp
+++ 归并排序和快排/quicksortplus.cpp
@@ -41,15 +41,30 @@ int* netherlandsFlag(int arr[], int L, int R)
     }
     swap(arr, more, R);
     int res[]={less+1, more};
-    return res;
+    cout<<"space is = "<<res[0]<<res[1]<<endl;
+    int* b;
+    b=res; 
+    //for (int i=0; i<2; i++)          //All right!
+    //{
+    //	cout<<*(b+i)<<endl;
+    //}
+    return b;
 }
 
 int main() {
-    int arr[]={4,1,2,3};
-    int* ans=netherlandsFlag(arr, 0, 2);
+    int arr[]={4,1,3,3};
+    int arr2[2]={0};
+    int* ans;
+    ans=arr2;
+    ans=netherlandsFlag(arr, 1, 3);
     // for (int k=0; k<2; k++)
     // {
     //     cout<<ans[k]<<endl;
     // }
-    cout<<ans[0];
+    int len = 3;
+    cout<<1111111111111<<endl;
+    for (int i=0; i<2; i++)
+    {
+    	cout<<ans<<endl;
+    }
 }
